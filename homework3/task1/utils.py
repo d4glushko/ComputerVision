0f09c2e619b93fc82dd8a365e7b62ffd4bbd58fc
Dima Glushko <dima2492@gmail.com>
2019-07-10 23:42:39 +0300

    Implemented mean shift

---
+++ homework3/task1/utils.py
@@ -0,0 +1,8 @@
+config = {
+    'basketball': {
+        'channels': [0],
+        'filter_from': (40., 0., 0),
+        'filter_to': (70., 255., 100.),
+        'roi': (198, 214, 28, 82),
+    }
+}
