aefec18615aa441713b3d2673c4de79472ce7fe8
Dima Glushko <dima2492@gmail.com>
2019-07-21 16:54:50 +0300

    Added data

---
+++ homework3/task2/result.md
@@ -0,0 +1,11 @@
+# How to run
+`python .\main.py --dataset "panda"`
+
+Allowed datasets with preconfigured ROI and filters:
+- panda
+- basketball
+- biker
+
+Or add any other dataset in the `data` folder and add ROI and filters to the `utils.py`
+
+# Results
---
+++ homework3/task2/utils.py
@@ -0,0 +1,20 @@
+config = {
+    'basketball': {
+        'channels': [0],
+        'filter_from': (40., 0., 0),
+        'filter_to': (80., 255., 110.),
+        'roi': (198, 214, 34, 81),
+    },
+    'biker': {
+        'channels': [0],
+        'filter_from': (70., 0., 0),
+        'filter_to': (180., 255., 255.),
+        'roi': (262,94,16,26),
+    },
+    'panda': {
+        'channels': [2],
+        'filter_from': (0., 0., 0),
+        'filter_to': (180., 255., 100.),
+        'roi': (58,100,28,23),
+    }
+}
